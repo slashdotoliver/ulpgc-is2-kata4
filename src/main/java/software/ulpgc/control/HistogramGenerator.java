b7314fdd86edb7a03b0946dfe8286396ea658c88
Oliver Cabrera <oliver.cabrera101@alu.ulpgc.es>
2024-11-14 12:04:54 +0000

    Refactor HistogramGenerator
    add import static Collectors.groupingBy and Collectors.reducing

--- src/main/java/software/ulpgc/control/HistogramGenerator.java
+++ src/main/java/software/ulpgc/control/HistogramGenerator.java
@@ -1,19 +1,19 @@
 package software.ulpgc.control;
 
 import software.ulpgc.model.Histogram;
-import software.ulpgc.model.Title;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
-import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.reducing;
 
 public class HistogramGenerator {
     public static <T> Histogram generateBy(Function<T, String> mapper, List<T> elements, String title) {
         Map<String, Integer> histogram = elements.stream()
                 .map(mapper)
-                .collect(Collectors.groupingBy(text -> text, Collectors.reducing(0, text -> 1, Integer::sum)));
+                .collect(groupingBy(text -> text, reducing(0, text -> 1, Integer::sum)));
         return new MapHistogram(histogram, title);
     }
 
